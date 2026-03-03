e9e4acfd45d61e8c380c077836c9ef1f66e12dc5
thakuraparna237-dot <thakuraparna237@gmail.com>
2026-03-03 21:36:57 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Shivambroog'),
+    'name' => env('APP_NAME', 'Everest'),
 
     /*
     |--------------------------------------------------------------------------
